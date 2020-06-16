eab7394eccb8948e1a64daf400f5ca2352d1c481
Win Marn Oo <winmaroo96@gmail.com>
2020-06-16 23:45:56 +0630

    after addding vue to laravel blog pj

---
+++ app/Http/Controllers/WmoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Wmo;
+use Illuminate\Http\Request;
+
+class WmoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Wmo  $wmo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wmo $wmo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Wmo  $wmo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Wmo $wmo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Wmo  $wmo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Wmo $wmo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Wmo  $wmo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wmo $wmo)
+    {
+        //
+    }
+}
